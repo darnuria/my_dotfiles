5280237020b44aaba966c1e772c5b0ac4c23a547
Axel Viala <axel.viala@darnuria.eu>
2019-02-06 21:46:07 +0100

    Add ls color in install.sh.

--- install.sh
+++ install.sh
@@ -3,24 +3,26 @@
 install_dir=`pwd`
 
 function make_symlinks() {
-    echo "Making symlinks"
-    cd $HOME
-    ln -sf $install_dir/bashrc.sh        .bashrc
-    ln -sf $install_dir/zshrc            .zshrc
-    ln -sf $install_dir/vimrc            .vimrc
-    ln -sf $install_dir/vim              .vim
-    ln -sf $install_dir/tmux.conf        .tmux.conf
-    ln -sf $install_dir/gitconfig        .gitconfig
-    ln -sf $install_dir/profile.sh       .profile
-    ln -sf $install_dir/gitignore_global .gitignore_global
-    ln -sf $install_dir/bash_logout.sh   .bash_logout
+  echo "Making symlinks"
+  cd $HOME
+  ln -sf $install_dir/bashrc.sh        .bashrc
+  ln -sf $install_dir/zshrc            .zshrc
+  ln -sf $install_dir/vimrc            .vimrc
+  ln -sf $install_dir/vim              .vim
+  ln -sf $install_dir/tmux.conf        .tmux.conf
+  ln -sf $install_dir/gitconfig        .gitconfig
+  ln -sf $install_dir/profile.sh       .profile
+  ln -sf $install_dir/gitignore_global .gitignore_global
+  ln -sf $install_dir/bash_logout.sh   .bash_logout
+  # Ls colors
+  ln -sf $install_dir/.dircolors.256dark .dircolors.256dark
 
-    #ln -sf $install_dir/tmux.conf .tmux.conf
+  #ln -sf $install_dir/tmux.conf .tmux.conf
 }
 
 function main() {
-    echo "Install Dotfiles!"
-    make_symlinks
+  echo "Install Dotfiles!"
+  make_symlinks
 }
 
 main
